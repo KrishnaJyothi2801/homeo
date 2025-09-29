bf4d25625b613934a745947709784004f4e5c5ae
Krishna Jyothi <krishna.jyothi@lightcast.io>
2025-09-29 15:41:42 +0530

    vercel json

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export", // ✅ tells Next.js to generate static HTML in `out/`
   images: {
     unoptimized: true, // ✅ required since Next.js image optimization needs a server
   },
