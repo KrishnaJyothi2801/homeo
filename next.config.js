79fd04fd70c5abc0b34b2ed4f3135ccbddb88e77
Sivaramakrishnan Narayanan <shivani.kj28.1@gmail.com>
2025-10-16 00:09:38 +0530

    build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** next.config.js */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export', // <-- enable static HTML export
   images: {
     domains: [
       "png.pngtree.com",
