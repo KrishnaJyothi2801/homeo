ee60211f66e2402f23b17d31b77e3c3bbe8d889b
KrishnaJyothi2801 <shivani.kj28.1@gmail.com>
2025-10-26 20:18:32 +0530

    Remove trailing slash from siteUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://drsivashomeo.vercel.app/',
+  siteUrl: 'https://drsivashomeo.vercel.app',
   generateRobotsTxt: true,
+  outDir: './public',
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
